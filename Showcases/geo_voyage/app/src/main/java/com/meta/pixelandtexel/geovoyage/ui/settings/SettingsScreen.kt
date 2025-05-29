d2f498a000cb3a593b988b22dc386243bc67472d
Spencer E <spencer@pixelandtexel.com>
2025-05-29 16:38:39 -0400

    Moved settings list items to separate files

--- Showcases/geo_voyage/app/src/main/java/com/meta/pixelandtexel/geovoyage/ui/settings/SettingsScreen.kt
+++ Showcases/geo_voyage/app/src/main/java/com/meta/pixelandtexel/geovoyage/ui/settings/SettingsScreen.kt
@@ -4,20 +4,16 @@ package com.meta.pixelandtexel.geovoyage.ui.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableIntState
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,8 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.meta.pixelandtexel.geovoyage.BuildConfig
@@ -36,11 +30,10 @@ import com.meta.pixelandtexel.geovoyage.enums.LlamaServerType
 import com.meta.pixelandtexel.geovoyage.enums.SettingsKey
 import com.meta.pixelandtexel.geovoyage.services.SettingsService
 import com.meta.pixelandtexel.geovoyage.ui.components.panel.SecondaryPanel
+import com.meta.pixelandtexel.geovoyage.ui.components.settings.SettingsRadioButtonGroupListItem
+import com.meta.pixelandtexel.geovoyage.ui.components.settings.SettingsSwitchListItem
+import com.meta.pixelandtexel.geovoyage.ui.components.settings.SettingsTextFieldListItem
 import com.meta.pixelandtexel.geovoyage.ui.theme.GeoVoyageTheme
-import com.meta.spatial.uiset.control.SpatialRadioButton
-import com.meta.spatial.uiset.control.SpatialSwitch
-import com.meta.spatial.uiset.input.SpatialTextField
-import com.meta.spatial.uiset.theme.LocalTypography
 
 @Composable
 fun SettingsScreen() {
@@ -90,7 +83,7 @@ fun SettingsScreen() {
                 ?: throw Exception("Invalid LlamaServerType from value $it")
             SettingsService.set(SettingsKey.LLAMA_SERVER_TYPE, newType.value)
           }
-          if (true || llamaServerSelectedIndex.intValue == LlamaServerType.OLLAMA.value) {
+          if (llamaServerSelectedIndex.intValue == LlamaServerType.OLLAMA.value) {
             SettingsTextFieldListItem(
               "Ollama server URL",
               stringResource(R.string.placeholder_ollama_url),
@@ -113,106 +106,6 @@ fun SettingsScreen() {
       }
     }
   }
-}
-
-@Composable
-fun SettingsSwitchListItem(
-  text: String,
-  enabled: MutableState<Boolean>,
-  onSettingChanged: (newValue: Boolean) -> Unit
-) {
-  Row(
-    horizontalArrangement = Arrangement.SpaceBetween,
-    verticalAlignment = Alignment.CenterVertically,
-    modifier = Modifier
-      .padding(12.dp, 0.dp)
-      .fillMaxWidth()
-  ) {
-    Text(
-      text = text,
-      style = LocalTypography.current.headline3,
-    )
-    SpatialSwitch(
-      checked = enabled.value,
-      onCheckedChange = { checked ->
-        enabled.value = checked
-        onSettingChanged(checked)
-      })
-  }
-  HorizontalDivider(color = Color.White)
-}
-
-@Composable
-fun SettingsRadioButtonGroupListItem(
-  headlineText: String,
-  selectedIdx: MutableIntState,
-  options: List<String>,
-  onOptionSelected: (idx: Int) -> Unit
-) {
-  Column(verticalArrangement = Arrangement.SpaceBetween) {
-    Row(
-      modifier = Modifier
-        .padding(start = 12.dp, end = 12.dp, bottom = 8.dp)
-        .fillMaxWidth()
-    ) {
-      Text(
-        text = headlineText,
-        style = LocalTypography.current.headline3
-      )
-    }
-    options.forEachIndexed { index, label ->
-      Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-          .padding(24.dp, 8.dp)
-          .fillMaxWidth()
-      ) {
-        Text(label, style = LocalTypography.current.body1)
-        SpatialRadioButton(
-          selected = index == selectedIdx.intValue,
-          onClick = {
-            selectedIdx.intValue = index
-            onOptionSelected(index)
-          }
-        )
-      }
-    }
-  }
-  HorizontalDivider(color = Color.White)
-}
-
-@Composable
-fun SettingsTextFieldListItem(
-  label: String,
-  placeholder: String,
-  textFieldValue: MutableState<String>,
-  onChanged: (String) -> Unit
-) {
-  Row(
-    modifier = Modifier
-      .padding(start = 24.dp, end = 12.dp)
-      .fillMaxWidth()
-  ) {
-    SpatialTextField(
-      label = label,
-      value = textFieldValue.value,
-      placeholder = placeholder,
-      onValueChange = {
-        textFieldValue.value = it
-      },
-      onSubmit = {
-        onChanged(it)
-      },
-      singleLine = true,
-      autoCorrectEnabled = false,
-      autoValidate = false,
-      capitalization = KeyboardCapitalization.None,
-      keyboardType = KeyboardType.Uri,
-      modifier = Modifier.fillMaxWidth()
-    )
-  }
-  HorizontalDivider(color = Color.White)
 }
 
 @Preview(widthDp = 570, heightDp = 480, showBackground = true, backgroundColor = 0xFFECEFE8)
